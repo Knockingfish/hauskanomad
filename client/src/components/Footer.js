f6666dd580bd10e062ecc350935870948ccd7fa2
Amin <alamingenetics@gmail.com>
2024-02-02 22:57:49 +0200

    Added Adress in Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,12 @@
-// A
+
 import React from 'react';
 
 const Footer = () => {
   return (
     <footer>
       <p>Contact us: info@hauskanomad.com</p>
+      <p>Phone: 045454989</p>
+      <p>Adress: Helsinki, Finland</p>
       <div className="social-links">
         <a href="https://facebook.com">Facebook</a>
         <a href="https://twitter.com">Twitter</a>
