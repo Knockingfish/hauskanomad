4e8bb98fb8423542adc315aab041df4bc4d74aaf
ReepZ <matthewjaveapillanes10@gmail.com>
2024-02-10 14:51:06 +0200

    Implemented About us :D

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import DestinationPage from './pages/DestinationPage';
 import ProfilePage from './pages/ProfilePage';
+import Aboutus from './pages/Aboutus';
 
 // Used purely to enable multiple pages and navigate between them
 
@@ -15,6 +16,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/About" element={<Aboutus />} />
           <Route path="/Destination" element={<DestinationPage />} />
           <Route path="/Profile" element={<ProfilePage />} />
         </Routes>
