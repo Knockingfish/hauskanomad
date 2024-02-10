d293fbaa86b0895601055ccc56e63a402d19c197
Knockingfish <william.fabian@protonmail.com>
2024-02-10 20:13:13 +0200

    Removed erroneous import

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
 import Home from './pages/Home';
 import DestinationPage from './pages/DestinationPage';
 import ProfilePage from './pages/ProfilePage';
@@ -11,7 +10,6 @@ function App() {
   return (
     <Router>
       <div>
-        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/About" element={<Aboutus />} />
