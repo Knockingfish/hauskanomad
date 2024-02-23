c671186d0eaff52bfb032657b0b6c47646400a61
ReepZ <matthewjaveapillanes10@gmail.com>
2024-02-23 16:51:02 +0200

    Created the destination page CSS which will be cloned in the future for the other destination pages.

--- client/src/pages/destinationpage/BookingForm.js
+++ client/src/pages/destinationpage/BookingForm.js
@@ -1,31 +1,25 @@
-
+import styles from './BookingForm.module.css'
 import React, { useState } from 'react';
 
-const BookingForm = ({ onSubmit }) => {
-  const [selectedDate, setSelectedDate] = useState('');
-  const [guests, setGuests] = useState(1);
-  const [rooms, setRooms] = useState(1);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSubmit({ selectedDate, guests, rooms });
-  };
+const BookingForm = () => {
+  
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Select Date:
-        <input type="date" value={selectedDate} onChange={(e) => setSelectedDate(e.target.value)} />
-      </label>
-      <label>
-        Number of Guests:
-        <input type="number" value={guests} onChange={(e) => setGuests(e.target.value)} />
-      </label>
-      <label>
-        Required Rooms:
-        <input type="number" value={rooms} onChange={(e) => setRooms(e.target.value)} />
-      </label>
-      <button type="submit">Book Now</button>
+    <form className={styles.outer_container}>
+      <div className={styles.form_container}>
+        <div className={styles.input_container}>
+          <input
+            placeholder="dunno"
+          />
+          <input
+            placeholder="wut dis"
+          />
+          <input
+            placeholder="who dat"
+          />
+          <button className={styles.form_button} type="submit">BOOK NOW</button>
+        </div>
+      </div>
     </form>
   );
 };
