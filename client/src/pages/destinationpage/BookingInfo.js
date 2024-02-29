5d223ba2754a4b0102079ce60a4c7c7d246fc967
knockingfish <william.fabian@protonmail.com>
2024-02-29 11:53:23 +0200

    reimplemented some improved CSS

--- client/src/pages/destinationpage/BookingInfo.js
+++ client/src/pages/destinationpage/BookingInfo.js
@@ -1,19 +1,17 @@
-
 import React from 'react';
 import styles from './BookingInfo.module.css';
 
 const BookingInfo = () => {
   return (
     <div className={styles.booking_container}>
-      <div className={styles.booking_outer}>
-        
-      <h2 className={styles.header}>Booking Information</h2>
+      <div className={styles.booking_bg}>
+        <div className={styles.header}>Booking Information</div>
         <div className={styles.booking_info}>
-          <div className={styles.booking_inner}>
+          <div className={styles.listing}>
             <p>Price: </p>
             <p>Capacity:</p>
           </div>
-          <div className={styles.booking_inner}>
+          <div className={styles.listing}>
             <p>Available Dates: </p>
             <p>Restrictions: </p>
           </div>
